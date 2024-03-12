2d84045744ea08a75efe4f084ef6131d660f92eb
Utkarsh <utkarshgayguwal123@gmail.com>
2024-03-12 17:23:47 +0530

    Created CSS folder to separate out css code.

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
         <div class="nav">
             <ul>
                 <li><a href="Home.php"><img class="my_img" src="img/home_white_24dp.svg" >Home</a></li>
-                <li><a href="Products.php"><img class="my_img" src="img/apartment_white_24dp.svg" >Products</a></li>
+                <li><a href="Products.php"><img class="my_img" src="img/apartment_white_24dp.svg">Products</a></li>
                 <li><a href="About.php"><img class="my_img" src="img/account_box_white_24dp.svg" >About Us</a></li>
                 <li><a href="Contact.php"><img class="my_img"
                   src="img/outline_connect_without_contact_white_24dp.png">Contact Us</a></li>
--- includes/links.php
+++ includes/links.php
@@ -1,6 +1,5 @@
-<link rel="stylesheet" href="bootstrap.css">
-<link rel="stylesheet" href="style2.css">
-<link rel="stylesheet" href="dec1321.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
 <!-- Link For Carousal -->
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> -->
 <!-- link for font -->
